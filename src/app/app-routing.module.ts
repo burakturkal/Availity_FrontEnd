73ba24b025310f20fab24a45289d5ab269839bc3
burakturkal <33044764+burakturkal@users.noreply.github.com>
2021-02-20 12:43:49 -0500

    Project added from local

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SecondComponent} from './second/second.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '' , component: AppComponent },
+  { path: 'second', component: SecondComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SecondComponent } from './second/second.component'
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+import { MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatButtonModule,
+  MatProgressSpinnerModule } from '@angular/material';
+
+  @NgModule({
+    declarations: [
+      AppComponent,
+      SecondComponent,
+    ],
+    imports: [
+      BrowserModule,
+      AppRoutingModule,
+      // HttpClientModule,
+      // BrowserAnimationsModule,
+      MatToolbarModule,
+      MatIconModule,
+      MatButtonModule,
+      MatCardModule,
+      MatProgressSpinnerModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+  })
+  export class AppModule { }
